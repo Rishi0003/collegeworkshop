565079ae74875723af92c782287aa7969e1aa5f5
rishi <sair8120@gmail.com>
2023-08-05 12:52:26 +0530

    Added another record

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -15,19 +15,27 @@ public class StudentService {
 
     public void addStudent(){
         Student student = new Student();
+        Student student1 = new Student();
         List<Student> studentList = new ArrayList<Student>();
         student.setStudentId("001");
         student.setName("name");
         student.setCourse("MCA");
         student.setStartDate("01/08/2023");
         student.setEndDate("08/01/2026");
-        studentList.add(student);
+// second student
+        student.setStudentId("002");
+        student.setName("Rishi");
+        student.setCourse("MCA");
+        student.setStartDate("01/08/2023");
+        student.setEndDate("08/01/2026");
+        studentList.add(student1);
 
 
 
     }
 
     public List<Student> getStudents(){
+
         return new ArrayList<Student>();
     }
 }
